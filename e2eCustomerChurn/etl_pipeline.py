76be9442dc39e5e10bc9b143a064155cb5a02b9d
Lusanda Mavuma <lusanda.mavuma@interfront.co.za>
2025-07-10 12:04:56 +0200

    Database connects successfully

--- e2eCustomerChurn/etl_pipeline.py
+++ e2eCustomerChurn/etl_pipeline.py
@@ -8,10 +8,11 @@ import os
 load_dotenv()
 
 # --- Database connection parameters--
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "customer_churn_db")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "superuser")
+print(DB_USER, DB_PASSWORD)
 
 
 # connect to the PostgreSQL database
